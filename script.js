94d560e72fe5c394f94364d9717edaaeea21fb7d
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-09-28 18:32:26 -0500

    Paint each square inside the grid
    By adding an event listener to each div created that's inside the
    container, when the mouse enters each div, it gets painted black.
    Additionally on the CSS a transition of 0.3s occur to make it look
    smoother.

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const gridGen = document.getElementById("main-grid")
 let gridSize = 16
 let cellSize = 480/gridSize
 
-gridGen.style.width = `480px`
+gridGen.style.width = "480px"
 
 for(i = 0; i < gridSize; i++){
     for(j = 0; j < gridSize; j++){
         const divG = document.createElement("div")
         divG.style.width = `${cellSize}px`
         divG.style.height = `${cellSize}px`
+
         divG.classList.add("grid-h")
+        divG.addEventListener("mouseenter", () => {
+            divG.style.background = 'black'
+        })
+
         gridGen.appendChild(divG)
     }
 }
+
