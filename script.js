2c054fd1b80d606c2a142a0b4c82a398eeccbaab
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-09-28 15:16:12 -0500

    Generate grid and calculate container size
    Using two "for" loops, one for width and one for height, the grid was
    generated. Since flexbox is being used, using a wrap is enough to
    have each div accomodate itself inside the container with wrap. After
    that it is only necessary to calculate the width of the container by
    multiplying the size of each square with the size of the grid

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const gridGen = document.getElementById("main-grid")
+let gridSize = 16
+let cellSize = 30
+const fullSize = gridSize * cellSize
 
+gridGen.style.width = `${fullSize}px`
+
+for(i = 0; i < gridSize; i++){
+    for(j = 0; j < gridSize; j++){
+        const divG = document.createElement("div")
+        divG.style.width = `${cellSize}px`
+        divG.style.height = `${cellSize}px`
+        divG.classList.add("grid-h")
+        gridGen.appendChild(divG)
+    }
+}
