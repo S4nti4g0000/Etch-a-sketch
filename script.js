1f2e5dd14515466e206fa30631fe3fb2daace62f
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-09-30 18:16:25 -0500

    Add buttons to reset and change grid size
    After confirming that each button can take an event listener,
    a few functions were created to be able to make the necessary changes:
    
    First, the code to generate the grid was put inside a function
    expression, this way the grid can be generated again with a new size
    from any part of the code.
    
    Second, the reset function, which simply uses a selector to go
    through all the divs and change the background of each one to white.
    
    Third, the function to change the grid size which takes in a prompt
    then it checks if the value that was entered is valid and doesn't
    exceed the limit, if so, the current grid will be deleted and a new
    one will be generated with the new size. Otherwise, the user will
    receive a new prompt to enter a new valid number.

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
 const gridGen = document.getElementById("main-grid")
 const buttonSelector = document.getElementById("buttons").querySelectorAll("button")
 let gridSize = 16
-let cellSize = 480/gridSize
+
 
 gridGen.style.width = "480px"
 
-for(i = 0; i < gridSize; i++){
-    for(j = 0; j < gridSize; j++){
-        const divG = document.createElement("div")
-        divG.style.width = `${cellSize}px`
-        divG.style.height = `${cellSize}px`
+const generateGrid = () => {
 
-        divG.classList.add("grid-h")
-        divG.addEventListener("mouseenter", () => {
-            divG.style.background = 'black'
-        })
+let cellSize = 480/gridSize
 
-        gridGen.appendChild(divG)
+    for(i = 0; i < gridSize; i++){
+        for(j = 0; j < gridSize; j++){
+            const divG = document.createElement("div")
+            divG.style.width = `${cellSize}px`
+            divG.style.height = `${cellSize}px`
+
+            divG.classList.add("grid-h")
+            divG.addEventListener("mouseenter", () => {
+                divG.style.background = 'black'
+            })
+
+            gridGen.appendChild(divG)
+        }
     }
 }
+
+generateGrid()
 
 
 buttonSelector.forEach((button) => {
     button.addEventListener("click", (ev) =>{
         let tar = ev.target
-        console.log(tar)
+        
+        switch(tar.id){
+            case 'bt-reset':
+                clearGrid()
+                break;
+            case 'bt-change':
+                changeSize()
+                break;
+        }
     })
 })
+
+
+const clearGrid = ()=> {
+    const selector = gridGen.querySelectorAll("div")
+    selector.forEach((d) => {
+        d.style.background = "white"
+    })
+}
+
+const changeSize = ()=>{
+    let newSize = prompt(`Enter a new grid size. Current size is ${gridSize}x${gridSize} 
+Max grid size: 64x64`)
+
+    if (newSize <= 64 && newSize >= 1){
+        const selector = gridGen.querySelectorAll("div")
+        selector.forEach((d) => {gridGen.removeChild(d)})
+        gridSize = +newSize      
+        generateGrid()
+    }else if(newSize > 64 || newSize < 0){
+        newSize = prompt('Invalid number. Please enter a new value between 1 and 64')
+    }    
+}
 
